cd295b2019294a09bf40bac20aa72d44e2aacd64
AsiaZhang <17612156438@sina.cn>
2018-05-11 11:07:57 +0800

    init commit

---
+++ ConvertDigitalAmountToChineseCapitalization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConvertDigitalAmountToChineseCapitalization
+//
+//  Created by joe on 2018/5/11.
+//  Copyright © 2018年 智富金融. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
